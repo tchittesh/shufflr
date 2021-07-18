0de736fa03b4032d794950425d6ed02aada17bbd
Chittesh Thavamani <tchittesh@gmail.com>
2021-07-18 17:30:03 -0400

    Added background image. Created an assets folder

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/logo.png';
 
 import './Header.css';
 
@@ -10,7 +11,7 @@ class Header extends PureComponent {
         <Navbar.Brand className="mx-auto">
           <img
             alt="Shufflr"
-            src="/logo.png"
+            src={logo}
             className="img-fluid-vertical mx-auto d-block"
             height="75"
           />
