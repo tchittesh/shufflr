905e5a5db38cbb4d441aa1a66e662062ad752f91
Dwaipayan Saha <dsaha@princeton.edu>
2021-08-06 07:07:03 -0400

    logout alignment

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,25 +9,26 @@ import './Header.css';
 class Header extends PureComponent {
   render() {
     return (
-      <Navbar bg="primary" variant="primary" fixed="top" className="justify-content-center">
-        <Navbar.Brand className="mx-auto">
-          <div className="flex flex-row justify-between">
-            <div style={{ width: 100 }}>
-              Placeholder
-            </div>
-            <Link to="/">
-              <img
-                alt="Shufflr"
-                src={logo}
-                className="img-fluid-vertical mx-auto d-block"
-                height="75"
-              />
-            </Link>
-            <Button variant="primary" type="button" className="topPadded" style={{ width: 100 }}>
-              Log Out
-            </Button>
-          </div>
-        </Navbar.Brand>
+      <Navbar className="d-flex justify-content-between" bg="primary" variant="primary" fixed="top" style={{ paddingTop: 10, paddingBottom: 10 }}>
+        <div style={{ width: 100, marginLeft: 20 }} />
+        <Link to="/">
+          <img
+            alt="Shufflr"
+            src={logo}
+            className="img-fluid-vertical mx-auto d-block"
+            height="75"
+          />
+        </Link>
+        <Button
+          variant="primary"
+          type="button"
+          className="topPadded"
+          style={{
+            width: 100, float: 'right', marginRight: 20, backgroundColor: 'purple',
+          }}
+        >
+          Log Out
+        </Button>
       </Navbar>
     );
   }
