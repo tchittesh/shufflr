955f288c23f3a8f301cedd9172df71b3488cf4ee
Chittesh Thavamani <tchittesh@gmail.com>
2021-08-07 00:15:01 -0400

    Simpler email solution, doesn't retrieve new access token per email

--- server/services/email.js
+++ server/services/email.js
@@ -1,49 +1,66 @@
 require('dotenv').config();
 const nodemailer = require('nodemailer');
-const {google} = require('googleapis');
+// const {google} = require('googleapis');
 
 
 // Chittesh: commented out more secure email API due to occasional breaking
 
-const createTransporter = async () => {
-  const oauth2Client = new google.auth.OAuth2(
-      process.env.GOOGLE_CLIENT_ID,
-      process.env.GOOGLE_CLIENT_SECRET,
-      'https://developers.google.com/oauthplayground',
-  );
-
-  oauth2Client.setCredentials({
-    refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
-  });
-
-  const accessToken = await new Promise((resolve, reject) => {
-    oauth2Client.getAccessToken((err, token) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(token);
-    });
-  });
-
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: process.env.EMAIL,
-      accessToken,
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-    },
-  });
-
-  return transporter;
-};
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    type: 'OAuth2',
+    user: process.env.EMAIL,
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+  },
+});
 
 const sendEmail = async (emailOptions) => {
-  const emailTransporter = await createTransporter();
-  await emailTransporter.sendMail(emailOptions);
+  await transporter.sendMail(emailOptions);
 };
+
+// const createTransporter = async () => {
+//   const oauth2Client = new google.auth.OAuth2(
+//       process.env.GOOGLE_CLIENT_ID,
+//       process.env.GOOGLE_CLIENT_SECRET,
+//       'https://developers.google.com/oauthplayground',
+//   );
+
+//   oauth2Client.setCredentials({
+//     refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+//   });
+
+//   const accessToken = await new Promise((resolve, reject) => {
+//     oauth2Client.getAccessToken((err, token) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(token);
+//     });
+//   });
+
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       type: 'OAuth2',
+//       user: process.env.EMAIL,
+//       accessToken,
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+//     },
+//   });
+
+//   return transporter;
+// };
+
+// const sendEmail = async (emailOptions) => {
+//   const emailTransporter = await createTransporter();
+//   await emailTransporter.sendMail(emailOptions);
+// };
 
 
 // Less secure version:
