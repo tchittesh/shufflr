88a39d7515140342b1f784c615d5351436b8c9b5
Dwaipayan Saha <dsaha@princeton.edu>
2021-08-16 20:04:45 -0400

    deleted sanjays space

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -8,22 +8,23 @@ import './Welcome.css';
 class Welcome extends PureComponent {
   render() {
     return (
-      <div>
-        <h1 className="title">Welcome to Shufflr!</h1>
-        <h5 className="intro">Shufflr is an app that allows students to have text conversations</h5>
-        <h5 className="intro">with random peers (similar to Omegle), restricted to a college environment.</h5>
-        <h5 className="intro">We want to help better connect our college community during times where we cannot</h5>
-        <h5 className="intro">meet in person, either due to COVID, or during holiday seasons.</h5>
-        <Col className="text-center">
-          <Link to="login">
-            <Button className="button-link" variant="purple">Log In</Button>
-            {' '}
-          </Link>
-          <Link to="create-account">
-            <Button className="button-link" variant="purple">Create Account</Button>
-          </Link>
-        </Col>
+      <div className="dflex-flex row">
+        <div>
+          <h1 className="intro">Chat with</h1>
+          <h1 className="intro">your peers,</h1>
+          <h1 className="intro">Make new</h1>
+          <h1 className="intro">friends</h1>
+          <Col className="text-left">
+            <Link to="login">
+              <Button className="button-link" variant="light">Log In</Button>
+            </Link>
+            <Link to="create-account">
+              <Button className="button-link" variant="light">Create Account</Button>
+            </Link>
+          </Col>
+        </div>
       </div>
+
     );
   }
 }
